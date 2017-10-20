c893c72faf2840e39ff57073f78d0206ec934016
Thomas Maurin <thomas@maurin.io>
2017-10-20 14:54:05 +0200

    fix packer dl url

--- scripts/install_packer.sh
+++ scripts/install_packer.sh
@@ -9,7 +9,7 @@ mkdir -p ${TOOLS_DIR}/bin
 if [ ! -d "${PACKER_DL}" ]
 then
     mkdir -p ${PACKER_DL}
-    wget -O packer-${PACKER_VERSION}.zip http://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
+    wget -O packer-${PACKER_VERSION}.zip https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
     unzip packer-${PACKER_VERSION}.zip -d ${PACKER_DL}
     rm packer-${PACKER_VERSION}.zip
 fi

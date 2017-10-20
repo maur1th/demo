b532962832a18c65e71cf84d78011672ba821156
Thomas Maurin <thomas@maurin.io>
2017-10-20 15:37:09 +0200

    remove route53

--- terraform/frontends/frontends.tf
+++ terraform/frontends/frontends.tf
@@ -87,16 +87,16 @@ resource "aws_autoscaling_group" "web_asg" {
     lifecycle { create_before_destroy = true }
 }
 
-resource "aws_route53_record" "web" {
-    count = "${var.route53_zoneid == "" ? 0: 1}"
-    zone_id = "${var.route53_zoneid}"
-    name = "${var.dns_alias}"
-    type = "A"
-    alias {
-        name = "${aws_elb.web.dns_name}"
-        zone_id = "${aws_elb.web.zone_id}"
-        evaluate_target_health = "false"
-    }
-}
+# resource "aws_route53_record" "web" {
+#     count = "${var.route53_zoneid == "" ? 0: 1}"
+#     zone_id = "${var.route53_zoneid}"
+#     name = "${var.dns_alias}"
+#     type = "A"
+#     alias {
+#         name = "${aws_elb.web.dns_name}"
+#         zone_id = "${aws_elb.web.zone_id}"
+#         evaluate_target_health = "false"
+#     }
+# }
 
 output "elb" { value = "${aws_elb.web.dns_name}" }
